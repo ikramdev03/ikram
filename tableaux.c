21504fb518c8868b173d77af36f94f1495b3296a
itachi <itachi@Itqchiis-MacBook-Pro.local>
2023-03-12 03:57:53 +0100

    add tab

---
+++ tableaux.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int main()
+ {
+    float T[10];
+    int i;
+    for(i=1;i<10;i++)
+    scanf("%f",&T[i]);
+
+    for(i=0;i<10;i++)
+    {
+    printf("\n%d",T[i]);
+    scanf("%d",&T[i]);
+    S=(float)S/N;
+    }
+     }
